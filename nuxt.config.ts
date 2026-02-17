0bbd64d74fa64dbba903a45b16897705b1361639
Growth Agent <growth@codecave.ar>
2026-02-17 20:29:10 +0000

    fix: disable prerender to fix stale HTML cache
    Route was prerendered at build time, serving old HTML

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   routeRules: {
-    '/': { prerender: true }
+    '/': { ssr: true }
   },
 
   compatibilityDate: '2025-01-15',
